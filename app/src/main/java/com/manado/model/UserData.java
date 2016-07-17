d97e6385cfdd3aa5369e653265eda21521341104
sdumasy <stephan_dumasy@icloud.com>
2016-07-17 16:04:05 +0200

    fixed error

--- app/src/main/java/com/manado/model/UserData.java
+++ app/src/main/java/com/manado/model/UserData.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * Created by macbookpro on 16-07-16.
  */
-public class UserData extends BaseModel{
+public class UserData{
     private ArrayList<User> data;
 
     public ArrayList<User> getData() {
